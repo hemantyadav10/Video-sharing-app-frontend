f7bd10baf6534d3d3fe94a978c939aac5679a9dc
Hemant <yad.heman@gmail.com>
2024-11-14 23:37:28 +0530

    create utility function toIndianDateFormat

---
+++ src/utils/utils.js
@@ -0,0 +1,15 @@
+function toIndianDateFormat(mongoDate) {
+  const date = new Date(mongoDate);
+
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = date.getFullYear();
+
+  return `${day}-${month}-${year}`;   
+
+}
+
+
+export {
+  toIndianDateFormat
+}
