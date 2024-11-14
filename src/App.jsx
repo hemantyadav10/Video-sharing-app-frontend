12e8b0023dd08454d32cf143f21c5298c32afc03
Hemant <yad.heman@gmail.com>
2024-11-15 00:07:06 +0530

    hide sidebar in dashboard route

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import React, { useState } from 'react'
 import Navbar from './components/Navbar.jsx'
 import Sidebar from './components/Sidebar.jsx'
 import BottomBar from './components/BottomBar.jsx'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 
 function App() {
   const [showMenu, setShowMenu] = useState(false)
-
+  const { pathname } = useLocation()
+  const isDashboardRoute = pathname === '/dashboard'
   const toggleMenu = () => setShowMenu(!showMenu)
 
   return (
     <div className='flex flex-col min-h-screen '>
       <Navbar toggleMenu={toggleMenu} />
       <div className='flex flex-1'>
-        <Sidebar showMenu={showMenu} toggleMenu={toggleMenu} />
-        <Outlet context={[showMenu]}/>
+        {!isDashboardRoute && <Sidebar showMenu={showMenu} toggleMenu={toggleMenu} />}
+        <Outlet context={[showMenu]} />
       </div>
       <BottomBar />
     </div>
