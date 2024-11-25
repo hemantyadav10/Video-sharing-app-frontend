2fec137e9cb8c5769c2befe799a1fe3bddb603fa
Hemant <yad.heman@gmail.com>
2024-11-25 23:59:28 +0530

    Implement auto-resizing textarea in channel tweet page with state-based element disabling.

--- src/components/TweetCard.jsx
+++ src/components/TweetCard.jsx
@@ -1,6 +1,6 @@
 import { DotsVerticalIcon, Pencil1Icon, TrashIcon } from '@radix-ui/react-icons'
 import { Avatar, Button, DropdownMenu, IconButton, Text, TextArea } from '@radix-ui/themes'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { timeAgo } from '../utils/formatTimeAgo'
 import ThumbsUpSolidIcon from '../assets/ThumbsUpSolidIcon'
 import ThumbsUp from '../assets/ThumbsUpIcon'
@@ -15,7 +15,7 @@ function TweetCard({
   channelId
 }) {
   const { user, isAuthenticated } = useAuth();
-  const { mutate: deleteComment } = useDeleteTweet(tweetData?._id, channelId)
+  const { mutateAsync: deleteComment, isPending: deletingComment } = useDeleteTweet(tweetData?._id, channelId)
   const navigate = useNavigate()
   const [tweetLikesCount, setTweetLikesCount] = useState(tweetData?.likesCount || 0)
   const [isTweetLiked, setIsTweetLiked] = useState(tweetData?.isLiked || false)
@@ -23,8 +23,13 @@ function TweetCard({
   const [isEditable, setIsEditable] = useState(false);
   const [content, setContent] = useState(tweetData?.content)
   const { mutate: updateTweet, isPending: updatingTweet } = useUpdateTweet(tweetData?._id, channelId)
+  const textareaRef = useRef(null);
 
-
+  const handleInput = () => {
+    const textarea = textareaRef.current;
+    textarea.style.height = "auto"; // Reset the height
+    textarea.style.height = `${textarea.scrollHeight}px`; // Set height based on scroll height
+  };
 
   useEffect(() => {
     if (tweetData?.isLiked !== undefined) {
@@ -33,10 +38,14 @@ function TweetCard({
   }, [tweetData?.isLiked]);
 
   const handleDeleteTweet = async () => {
-    deleteComment(tweetData?._id, {
-      onSuccess: () => {
-        toast('Tweet deleted')
-      }
+    toast.promise(
+      deleteComment(tweetData?._id),
+      {
+        loading: 'Deleting tweet...',
+        success: 'Tweet deleted',
+        error: 'Something went wrong, please try again.',
+      }, {
+      icon: false
     })
   }
 
@@ -69,6 +78,12 @@ function TweetCard({
     })
   }
 
+  useEffect(() => {
+    if (isEditable && textareaRef.current) {
+      handleInput(); // Adjust height when entering edit mode
+    }
+  }, [isEditable]);
+
   return (
     <div className='flex gap-3 pb-4 border border-[#484848] p-4 rounded-xl'>
       <Avatar
@@ -78,18 +93,26 @@ function TweetCard({
       />
       {isEditable && <div className='flex-1'>
         <TextArea
+          ref={textareaRef}
+          onInput={handleInput}
+          placeholder='Add a tweet...'
           value={content}
           onChange={(e) => setContent(e.target.value)}
+          autoFocus
+          disabled={updatingTweet}
         />
         <div className='flex justify-end gap-2 mt-2 '>
           <Button
             onClick={() => {
               setIsEditable(false)
-              setContent(comment?.content)
+              setContent(tweetData?.content)
             }}
             variant='soft'
             color='gray'
+            className='px-4'
+            radius='full'
             highContrast
+            disabled={updatingTweet}
           >
             Cancel
           </Button>
@@ -97,8 +120,9 @@ function TweetCard({
             onClick={handleUpdateTweet}
             disabled={!content?.trim() || content?.trim() === tweetData?.content}
             loading={updatingTweet}
-            variant='soft'
             highContrast
+            className='px-4'
+            radius='full'
           >
             Save
           </Button>
@@ -122,32 +146,38 @@ function TweetCard({
               {timeAgo(tweetData?.createdAt)}
             </Text>
           </div>
-          {tweetData?.owner._id === user?._id && <DropdownMenu.Root>
-            <DropdownMenu.Trigger>
-              <IconButton
-                variant='ghost'
+          {tweetData?.owner._id === user?._id &&
+            <DropdownMenu.Root>
+              <DropdownMenu.Trigger disabled={deletingComment}>
+                <IconButton
+                  variant='ghost'
+                  highContrast
+                  color='gray'
+                  radius='full'
+                  size={'2'}
+                >
+                  <DotsVerticalIcon height={'18px'} width={'18px'} />
+                </IconButton>
+              </DropdownMenu.Trigger>
+              <DropdownMenu.Content
+                variant='soft'
                 highContrast
-                color='gray'
-                radius='full'
-                size={'2'}
+                className='w-48'
               >
-                <DotsVerticalIcon />
-              </IconButton>
-            </DropdownMenu.Trigger>
-            <DropdownMenu.Content variant='soft'>
-              <DropdownMenu.Item onClick={() => setIsEditable(true)}>
-                <Pencil1Icon /> Edit
-              </DropdownMenu.Item>
-              <DropdownMenu.Item onClick={handleDeleteTweet}>
-                <TrashIcon /> Delete
-              </DropdownMenu.Item>
-            </DropdownMenu.Content>
-          </DropdownMenu.Root>
+                <DropdownMenu.Item onClick={() => setIsEditable(true)}>
+                  <Pencil1Icon /> Edit
+                </DropdownMenu.Item>
+                <DropdownMenu.Item color='red' onClick={handleDeleteTweet}>
+                  <TrashIcon /> Delete
+                </DropdownMenu.Item>
+              </DropdownMenu.Content>
+            </DropdownMenu.Root>
           }
         </div>
 
         <Text
           as='p'
+          size={'2'}
           className='pr-4 break-words whitespace-pre-wrap'
         >
           {tweetData?.content}
@@ -159,6 +189,7 @@ function TweetCard({
             color='gray'
             highContrast
             radius='full'
+            disabled={deletingComment}
           >
             {(isAuthenticated && isTweetLiked) ?
               <ThumbsUpSolidIcon height='20' width='20' /> :
@@ -175,4 +206,4 @@ function TweetCard({
   )
 }
 
-export default TweetCard
+export default React.memo(TweetCard)
--- src/pages/channel/ChannelTweets.jsx
+++ src/pages/channel/ChannelTweets.jsx
@@ -1,5 +1,5 @@
 import { Button, Spinner, TextArea } from '@radix-ui/themes'
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import TweetCard from '../../components/TweetCard'
 import { useCreateTweet, useFetchUserTweets } from '../../lib/queries/tweetQueries'
 import { useOutletContext } from 'react-router-dom'
@@ -12,6 +12,13 @@ function ChannelTweets() {
   const { data: userTweets, isLoading } = useFetchUserTweets(userId, user?._id)
   const { mutate: createTweet, isPending: creatingTweet } = useCreateTweet(user, userId)
   const [content, setContent] = useState('')
+  const textareaRef = useRef(null);
+
+  const handleInput = () => {
+    const textarea = textareaRef.current;
+    textarea.style.height = "auto"; // Reset the height
+    textarea.style.height = `${textarea.scrollHeight}px`; // Set height based on scroll height
+  };
 
   const handleCreateTweet = async () => {
     createTweet(content, {
@@ -27,35 +34,48 @@ function ChannelTweets() {
   return (
     <div>
       {/* Text area to write a tweet */}
-      {user?._id === userId && <div className='flex-1 max-w-4xl'>
-        <TextArea
-          placeholder='Add a tweet...'
-          value={content}
-          onChange={e => setContent(e.target.value)}
-        />
-        <div className='flex justify-end gap-2 mt-2 '>
-          <Button
-            onClick={() => {
-              setContent('')
-            }}
-            hidden={!content?.trim()}
-            variant='soft'
-            color='gray'
-            highContrast
-          >
-            Cancel
-          </Button>
-          <Button
-            onClick={handleCreateTweet}
-            disabled={!content?.trim()}
-            loading={creatingTweet}
-            variant='soft'
-            highContrast
-          >
-            Send
-          </Button>
+      {(isAuthenticated && user?._id === userId) &&
+        <div className='flex-1 max-w-4xl'>
+          <TextArea
+            ref={textareaRef}
+            placeholder='Add a tweet...'
+            value={content}
+            onChange={e => setContent(e.target.value)}
+            onInput={handleInput}
+            disabled={creatingTweet}
+            autoFocus
+            className='peer'
+          />
+          <div className='flex justify-end gap-2 mt-2 '>
+            <Button
+              onClick={() => {
+                setContent('')
+                const textarea = textareaRef.current;
+                textarea.style.height = "auto"; // Set height based on scroll height
+
+              }}
+              hidden={!content?.trim()}
+              variant='soft'
+              color='gray'
+              highContrast
+              className='px-4'
+              radius='full'
+              disabled={creatingTweet}
+            >
+              Cancel
+            </Button>
+            <Button
+              onClick={handleCreateTweet}
+              disabled={!content?.trim()}
+              loading={creatingTweet}
+              highContrast
+              className='px-4'
+              radius='full'
+            >
+              Send
+            </Button>
+          </div>
         </div>
-      </div>
       }
       {/* tweet cards */}
       <div className='flex flex-col max-w-4xl gap-6 mt-6'>
